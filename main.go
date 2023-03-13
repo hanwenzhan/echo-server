41cd157a57c18f97b993400cdd4becdcfff7f32b
hanwen <hanwen@icetech.com.tw>
2023-03-13 11:37:09 +0800

    update

--- main.go
+++ main.go
@@ -16,16 +16,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/line/line-bot-sdk-go/v7/linebot"
-	"github.com/line/line-bot-sdk-go/v7/linebot/httphandler"
 )
 
 func main() {
-	handler, err := httphandler.New(
+	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_TOKEN"),
 	)
@@ -34,10 +34,15 @@ func main() {
 	}
 
 	// Setup HTTP Server for receiving requests from LINE platform
-	handler.HandleEvents(func(events []*linebot.Event, r *http.Request) {
-		bot, err := handler.NewClient()
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
 		if err != nil {
-			log.Print(err)
+			log.Print("callback:", err)
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
 			return
 		}
 		for _, event := range events {
@@ -45,13 +50,18 @@ func main() {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
+						log.Print("callback:", err)
+					}
+				case *linebot.StickerMessage:
+					replyMessage := fmt.Sprintf(
+						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print("callback:", err)
 					}
 				}
 			}
 		}
 	})
-	http.Handle("/callback", handler)
 
 	//
 	type Info struct {
